8ae4d86abf7c52e8f0862da32b35488c71c53359
duogxaolin <duogxaolin@gmail.com>
2026-07-25 13:48:49 +0700

    fix: remove mysqldump warning line from SQL dump

--- migrations/full_dump.sql
+++ migrations/full_dump.sql
@@ -1,4 +1,3 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 8.0.46, for Linux (aarch64)
 --
 -- Host: localhost    Database: cdkt_admin
